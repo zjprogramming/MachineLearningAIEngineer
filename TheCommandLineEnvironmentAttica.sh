6d68b2b1e702214cd230b0b82f195fa7f5f1d606
zjprogramming <145306561+zjprogramming@users.noreply.github.com>
2023-10-08 13:13:40 -0700

    Add files via upload

---
+++ TheCommandLineEnvironmentAttica.sh
@@ -0,0 +1,74 @@
+$ pwd
+/home/ccuser/workspace/clothing
+$ nano hello.txt
+$ nano ~/.bash_profile
+$ source ~/.bash_profile
+hello
+$ nano ~/.bash_profile
+$ source ~/.bash_profile
+hello
+$ p
+/home/ccuser/workspace/clothing
+$ ll
+total 4
+drwxr-xr-x 2 ccuser ccuser 152 Oct  8 20:01 .
+drwxr-xr-x 1 ccuser ccuser  22 Oct  8 19:59 ..
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 dresses.txt
+-rw-r--r-- 1 ccuser ccuser  17 Oct  8 20:01 hello.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 jackets.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 pants.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 scarves.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 shirts.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 socks.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 sweaters.txt
+$ nano ~/.bash_profile
+$ echo $USER
+
+$ source ~/.bash_profile
+hello
+$ echo $USER
+Zack
+$ nano ~/.bash_profile
+$ source ~/.bash_profile
+hello
+>>p
+/home/ccuser/workspace/clothing
+>>ll
+total 4
+drwxr-xr-x 2 ccuser ccuser 152 Oct  8 20:01 .
+drwxr-xr-x 1 ccuser ccuser  22 Oct  8 19:59 ..
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 dresses.txt
+-rw-r--r-- 1 ccuser ccuser  17 Oct  8 20:01 hello.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 jackets.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 pants.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 scarves.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 shirts.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 socks.txt
+-rw-r--r-- 1 ccuser ccuser   0 Sep 28 21:00 sweaters.txt
+>>echo $HOME
+/home/ccuser
+>>echo $PATH
+/home/ccuser/.bin:/home/ccuser/node_modules/.bin:/home/ccuser/.gem/ruby/2.3.0/bin:/home/ccuser/.composer/vendor/bin:/home/ccuser/.bin:/home/ccuser/node_modules/.bin:/home/ccuser/.gem/ruby/2.3.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+>>env
+HOSTNAME=84c4decf1794
+GEM_HOME=/home/ccuser/.gem/ruby/2.3.0
+TERM=xterm
+USER=Zack
+EXPIRES_AT=1696798747
+NLTK_DATA=/home/ccuser/.nltk_data
+PATH=/home/ccuser/.bin:/home/ccuser/node_modules/.bin:/home/ccuser/.gem/ruby/2.3.0/bin:/home/ccuser/.composer/vendor/bin:/home/ccuser/.bin:/home/ccuser/node_modules/.bin:/home/ccuser/.gem/ruby/2.3.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+CODEX_RUNNER_PATH=/var/codecademy/codex/runners
+PWD=/home/ccuser/workspace/clothing
+MPLBACKEND=agg
+LANG=en_US.UTF-8
+SESSION_ID=bebe5872-c5b2-43ee-b79d-8745d1f75c63
+TZ=Etc/UTC
+PS1=>>
+SHLVL=1
+HOME=/home/ccuser
+LOG_LEVEL=2
+WORKSPACE_SERVICE_HOST=https://workspace.production-eks.codecademy.com
+EIN_IMAGE=default
+PYTHONPATH=/var/codecademy/runner_contexts/python:
+_=/usr/bin/env
+>>
