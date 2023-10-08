e327ab6b5f3fc1a4db1c7d24da2ab22cd6c08890
zjprogramming <145306561+zjprogramming@users.noreply.github.com>
2023-10-08 13:30:02 -0700

    Add files via upload

---
+++ TheCommandLineLinguaFranca.sh
@@ -0,0 +1,97 @@
+$ pwd
+/home/ccuser/workspace/lingua-franca
+$ nano ~/.bash_profile
+$ source ~/.bash_profile
+This is my greeting
+$ nano ~/.bash_profile
+$ source ~/.bash_profile
+This is my greeting
+$ md text
+$ ls
+arabic.txt   japanese.txt  russian.txt  thai.txt
+bengali.txt  javanese.txt  spanish.txt  vietnamese.txt
+english.txt  korean.txt    tagalog.txt
+german.txt   mandarin.txt  tamil.txt
+hindi.txt    panjabi.txt   text
+$ d
+Sun Oct  8 20:22:31 UTC 2023
+$ hy
+    1  pwd
+    2  nano ~/.bash_profile
+    3  source ~/.bash_profile
+    4  nano ~/.bash_profile
+    5  source ~/.bash_profile
+    6  md test
+    7  clear
+    8  md test
+    9  source ~/.bash_profile
+   10  nano ~/.bash_profile
+   11  source ~/.bash_profile
+   12  md text
+   13  ls
+   14  d
+   15  hy
+$ less javanese.txt
+$ nano ~/.bash_profile
+$ $ source ~/.bash_profile
+This is my greeting
+$ less javanese.txt
+$ nano ~/.bash_profile
+$ source ~/.bash_profile
+This is my greeting
+----> md test1
+----> d
+Sun Oct  8 20:28:16 UTC 2023
+----> hy
+    1  pwd
+    2  nano ~/.bash_profile
+    3  source ~/.bash_profile
+    4  nano ~/.bash_profile
+    5  source ~/.bash_profile
+    6  md test
+    7  clear
+    8  md test
+    9  source ~/.bash_profile
+   10  nano ~/.bash_profile
+   11  source ~/.bash_profile
+   12  md text
+   13  ls
+   14  d
+   15  hy
+   16  less javanese.txt
+   17  nano ~/.bash_profile
+   18  clear
+   19  source ~/.bash_profile
+   20  clear
+   21  source ~/.bash_profile
+   22  less javanese.txt
+   23  nano ~/.bash_profile
+   24  source ~/.bash_profile
+   25  md test1
+   26  d
+   27  hy
+----> env
+HOSTNAME=414b896781f5
+GEM_HOME=/home/ccuser/.gem/ruby/2.3.0
+TERM=xterm
+EXPIRES_AT=1696799699
+NLTK_DATA=/home/ccuser/.nltk_data
+PATH=/home/ccuser/.bin:/home/ccuser/node_modules/.bin:/home/ccuser/.gem/ruby/2.3.0/bin:/home/ccuser/.composer/vendor/bin:/home/ccuser/.bin:/home/ccuser/node_modules/.bin:/home/ccuser/.gem/ruby/2.3.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+CODEX_RUNNER_PATH=/var/codecademy/codex/runners
+PWD=/home/ccuser/workspace/lingua-franca
+MPLBACKEND=agg
+LANG=en_US.UTF-8
+SESSION_ID=4d0d2fd9-a8ee-45d4-988b-ceeaaddd9285
+TZ=Etc/UTC
+PS1=----> 
+SHLVL=1
+HOME=/home/ccuser
+LOG_LEVEL=2
+WORKSPACE_SERVICE_HOST=https://workspace.production-eks.codecademy.com
+EIN_IMAGE=default
+LESS=-N
+PYTHONPATH=/var/codecademy/runner_contexts/python:
+_=/usr/bin/env
+----> env | grep LESS
+LESS=-N
+----> 
